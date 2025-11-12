9dcd441e9d06891620fd49826a4154ed4177662c
Beatstamosi <moritz.lued@gmail.com>
2025-11-12 16:39:22 +0000

    Fix: Session Image url not stored in db

--- client/src/components/AddSession/AddSession.tsx
+++ client/src/components/AddSession/AddSession.tsx
@@ -78,7 +78,7 @@ export default function AddSession() {
           forecast,
           shareInFeed,
           ...formValues,
-          image: imageUrl,
+          sessionImageUrl: imageUrl,
         }),
       });
 
