bbac0835ad8b33db77f158ba889641a7cf72cf0f
Beatstamosi <moritz.lued@gmail.com>
2025-12-22 15:52:16 +0000

    fix: update surflineClient
    Requests are getting blocked in production

--- server/src/utils/surflineClient.ts
+++ server/src/utils/surflineClient.ts
@@ -2,20 +2,15 @@ import axios from 'axios';
 
 export const surflineClient = axios.create({
   baseURL: 'https://services.surfline.com',
-  headers:  {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+  timeout: 10000, // Crucial for production
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
     'Accept': 'application/json, text/plain, */*',
     'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
     'Referer': 'https://www.surfline.com/',
     'Origin': 'https://www.surfline.com',
-    'Cache-Control': 'no-cache',
-    'Pragma': 'no-cache',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-site',
-    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"'
   }
 });
